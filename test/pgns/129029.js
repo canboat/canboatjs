7b69d63dbf6d9db185215e0997bba75809cb9353
Scott Bender <scott@scottbender.net>
2018-02-04 11:16:35 -0500

    fix: time convertion

--- test/pgns/129029.js
+++ test/pgns/129029.js
@@ -1,4 +1,4 @@
 module.exports = [{
-  "expected": {"timestamp":"2017-04-15T14:57:58.470Z","prio":3,"src":3,"dst":255,"pgn":129029,"description":"GNSS Position Data","fields":{"SID":155,"Date":"2017.04.15", "Time": "14:57:56.08950","Latitude":39.0700875,"Longitude":-76.4640319,"GNSS type":"GPS+SBAS/WAAS","Method":"DGNSS fix","Integrity":"No integrity checking","Number of SVs":18,"HDOP":0.65,"Geoidal Separation":-0.01,"list":[{"Reference Station ID":7}]}},
-  "input":"2017-04-15T14:57:58.470Z,3,129029,3,255,45,9b,77,43,36,f6,1c,20,00,2e,cf,33,60,0c,6c,05,00,4a,10,5d,ae,73,63,f5,ff,ff,ff,ff,ff,ff,ff,7f,23,fc,12,41,00,ff,7f,ff,ff,ff,7f,00,ff,ff"
+  "expected": {"timestamp":"2017-04-15T14:57:58.470Z","prio":3,"src":3,"dst":255,"pgn":129029,"description":"GNSS Position Data","fields":{"SID":155,"Date":"2017.04.15", "Time": "14:57:56.89500","Latitude":39.0700875,"Longitude":-76.4640319,"GNSS type":"GPS+SBAS/WAAS","Method":"DGNSS fix","Integrity":"No integrity checking","Number of SVs":18,"HDOP":0.65,"Geoidal Separation":-0.01,"list":[{"Reference Station ID":7}]}},
+  "input":"2017-04-15T14:57:58.470Z,3,129029,3,255,45,9b,77,43,36,f6,1c,20,00,2e,cf,33,60,0c,6c,05,ff,49,10,5d,ae,73,63,f5,ff,ff,ff,ff,ff,ff,ff,7f,23,fc,12,41,00,ff,7f,ff,ff,ff,7f,00,ff,ff"
 }]
