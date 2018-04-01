9894af57c175a5527517115aa1616a86cc009399
Scott Bender <scott@scottbender.net>
2018-03-31 20:54:36 -0400

    chore: update test for 127489

--- test/pgns/127489.js
+++ test/pgns/127489.js
@@ -1,4 +1,4 @@
 module.exports = [{
-  "expected": {"timestamp":"2017-04-15T14:57:58.469Z","prio":2,"src":17,"dst":255,"pgn":127489,"description":"Engine Parameters, Dynamic","fields":{"Engine Instance":"Single Engine or Dual Engine Port","Oil pressure":393000,"Temperature":330.43,"Alternator Potential":327.67,"Total Engine hours":699005,"Discrete Status 1": [],"Discrete Status 2": []}},
+  "expected": {"timestamp":"2017-04-15T14:57:58.469Z","prio":2,"src":17,"dst":255,"pgn":127489,"description":"Engine Parameters, Dynamic","fields":{"Engine Instance":"Single Engine or Dual Engine Port","Oil pressure":393000,"Temperature":330.43,"Total Engine hours":699005,"Discrete Status 1": [],"Discrete Status 2": []}},
   "input": "2017-04-15T14:57:58.469Z,2,127489,17,255,26,00,5a,0f,ff,ff,13,81,ff,7f,ff,7f,7d,aa,0a,00,ff,ff,ff,ff,ff,00,00,00,00,7f,7f"
 }]
