725d989eebda47ca17bf6a15051af4b72bc954b6
Scott Bender <scott@scottbender.net>
2020-02-08 12:10:57 -0500

    feature: add helmSmart support for ios

--- ios.js
+++ ios.js
@@ -1,18 +1,26 @@
 
 const FromPgn = require('./lib/fromPgn').Parser
-const pgnToYdwgRawFormat = require('./lib/toPgn').pgnToYdwgRawFormat
+const { pgnToYdgwRawFormat, actisenseToYdgwRawFormat, pgnToPCDIN, actisenseToPCDIN} = require('./lib/toPgn')
 
 const parser = new FromPgn({})
 
-global.parseYDWG02 = (pgn_data) => {
-  return parser.parseYDWG02(pgn_data)
+global.parseYDGW02 = (pgn_data) => {
+  return parser.parseYDGW02(pgn_data)
 }
 global.parseVenusMQTT = (pgn_data) => {
   return parser.parseVenusMQTT(pgn_data)
 }
+global.actisenseToYdgwRawFormat = actisenseToYdgwRawFormat
+global.pgnToYdgwRawFormat = pgnToYdgwRawFormat
+global.pgnToPCDIN = pgnToPCDIN
+global.actisenseToPCDIN = actisenseToPCDIN
+  
+global.parsePCDIN = (pcdin) => {
+  return parser.parseN2KOver0183(pcdin)
+}
 
-global.pgnToYdwgRawFormat = pgnToYdwgRawFormat
+global.parseHelmSmart = global.parsePCDIN
 
 //global.toPgn: require('./lib/toPgn').toPgn,
 
-//console.log(global.parseYDWG02('06:06:39.801 R 09F10DCC 00 FF FF 7F FF 7F FF FF'))
+//console.log(global.parseYDGW02('06:06:39.801 R 09F10DCC 00 FF FF 7F FF 7F FF FF'))
