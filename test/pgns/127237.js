1b2eadf618080d4340cd4342de9fff94e43d07fc
Scott Bender <scott@scottbender.net>
2020-03-07 13:15:04 -0500

    fix: update to handle the latest canboat pgns.json (#101)

--- test/pgns/127237.js
+++ test/pgns/127237.js
@@ -1,4 +1,4 @@
 module.exports = [{
-  "expected": {"timestamp":"2017-04-15T14:57:58.469Z","prio":2,"src":172,"dst":255,"pgn":127237,"description":"Heading/Track control","fields":{"Rudder Limit Exceeded":0,"Override":0,"Steering Mode":2,"Turn Mode":12,"Commanded Rudder Direction":0,"Commanded Rudder Angle":-0.0015}},
+  "expected": {"timestamp":"2017-04-15T14:57:58.469Z","prio":2,"src":172,"dst":255,"pgn":127237,"description":"Heading/Track control","fields":{"Rudder Limit Exceeded":"No","Override":"No","Steering Mode":2,"Turn Mode":"Rudder Limit controlled","Commanded Rudder Direction":"Move to starboard","Commanded Rudder Angle":-0.0015}},
   "input": "2017-04-15T14:57:58.469Z,2,127237,172,255,21,3c,c2,3f,f1,ff,ff,ff,ff,ff,ff,ff,ff,ff,ff,7f,ff,7f,ff,7f,ff,ff"
 }]
--- test/pgns/129283.js
+++ test/pgns/129283.js
@@ -1,4 +1,4 @@
 module.exports = [{
   "expected": {"timestamp":"2017-04-15T14:57:58.468Z","prio":3,"src":3,"dst":255,"pgn":129283,"description":"Cross Track Error","fields":{"XTE mode":"Autonomous","Navigation Terminated":"No","XTE":-0.75}},
-  "input": "2017-04-15T14:57:58.468Z,3,129283,3,255,6,ff,30,b5,ff,ff,ff"
+  "input": "2017-04-15T14:57:58.468Z,3,129283,3,255,8,ff,30,b5,ff,ff,ff,ff,ff"
 }]
