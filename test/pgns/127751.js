59562f85de2915e8963ec7a43c831fca08008038
Scott Bender <scott@scottbender.net>
2020-06-08 11:21:11 -0400

    tests: add test for 127751

---
+++ test/pgns/127751.js
@@ -0,0 +1,4 @@
+module.exports = [{
+  "expected": {"prio":2,"src":0,"dst":255,"pgn":127751,"fields":{"Connection Number":0,"DC Voltage":13,"DC Current":0.78},"description":"DC Voltage/Current"},
+  "input": "2020-06-08T15:19:42.887Z,2,127751,0,255,8,ff,00,82,00,4e,00,00,ff"
+}]
