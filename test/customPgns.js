809bc10e1fbf3ace02090c80615a0b3ec6ea2abc
Scott Bender <scott@scottbender.net>
2021-02-17 08:44:02 -0500

    fix: use new apis for registration

--- test/customPgns.js
+++ test/customPgns.js
@@ -4,18 +4,23 @@ chai.use(require('chai-things'))
 chai.use(require('chai-json-equal'));
 
 const { FromPgn, pgnToActisenseSerialFormat } = require('../index')
-const EventEmitter = require('events')
+const PropertyValues =  require('@signalk/server-api').PropertyValues
+
 
 describe('custom pgns', function () {
 
   it(`custom pgn in`, function (done) {
     
-    const emitter = new EventEmitter();
+    const propertyValues = new PropertyValues();
 
-    emitter.on('canboat-custom-pgn-available', () => {
-      emitter.emit('canboat-custom-pgn', definition)
+    propertyValues.emitPropertyValue({
+      timestamp: Date.now(),
+      setter: 'customPgns',
+      name: 'canboat-custom-pgns',
+      value: definitions
     })
-    var fromPgn = new FromPgn({}, emitter)
+    
+    var fromPgn = new FromPgn({}, propertyValues)
 
     fromPgn.on('error', (pgn, error) => {
       console.error(`Error parsing ${pgn.pgn} ${error}`)
@@ -51,21 +56,29 @@ describe('custom pgns', function () {
 
   it(`custom pgn callback works`, function (done) {
     
-    const emitter = new EventEmitter();
+    const propertyValues = new PropertyValues();
 
-    emitter.on('canboat-custom-pgn-available', () => {
-      emitter.emit('canboat-custom-pgn', definition, (pgn) => {
+    const defs = {
+      ...definitions,
+      callback: (pgn) => {
         try {
-          //console.log(JSON.stringify(pgn))
           delete pgn.input
           pgn.should.jsonEqual(expected)
           done()
         } catch ( e ) {
           done(e)
         }
-      })
+      }
+    }
+
+    propertyValues.emitPropertyValue({
+      timestamp: Date.now(),
+      setter: 'customPgns',
+      name: 'canboat-custom-pgns',
+      value: defs
     })
-    var fromPgn = new FromPgn({}, emitter)
+    
+    var fromPgn = new FromPgn({}, propertyValues)
 
     fromPgn.on('error', (pgn, error) => {
       console.error(`Error parsing ${pgn.pgn} ${error}`)
@@ -99,223 +112,227 @@ var expected = {
     "Commanded Rudder Angle":-0.0015
   }
 }
-const definition = {
-  "PGN":127999,
-  "Id":"headingTrackControl",
-  "Description":"Heading/Track control",
-  "Type":"Fast",
-  "Complete":true,
-  "Length":21,
-  "RepeatingFields":0,
-  "Fields":[
+const definitions = {
+  PGNs: [
     {
-      "Order":1,
-      "Id":"rudderLimitExceeded",
-      "Name":"Rudder Limit Exceeded",
-      "BitLength":2,
-      "BitOffset":0,
-      "BitStart":0,
-      "Type":"Lookup table",
-      "Signed":false,
-      "EnumValues":[
-        {"name":"No","value":"0"},
-        {"name":"Yes","value":"1"},
-        {"name":"Error","value":"10"},
-        {"name":"Unavailable","value":"11"}]},
-    {
-      "Order":2,
-      "Id":"offHeadingLimitExceeded",
-      "Name":"Off-Heading Limit Exceeded",
-      "BitLength":2,
-      "BitOffset":2,
-      "BitStart":2,
-      "Type":"Lookup table",
-      "Signed":false,
-      "EnumValues":[
-        {"name":"No","value":"0"},
-        {"name":"Yes","value":"1"},
-        {"name":"Error","value":"10"},
-        {"name":"Unavailable","value":"11"}]},
-    {
-      "Order":3,
-      "Id":"offTrackLimitExceeded",
-      "Name":"Off-Track Limit Exceeded",
-      "BitLength":2,
-      "BitOffset":4,
-      "BitStart":4,
-      "Type":"Lookup table",
-      "Signed":false,
-      "EnumValues":[
-        {"name":"No","value":"0"},
-        {"name":"Yes","value":"1"},
-        {"name":"Error","value":"10"},
-        {"name":"Unavailable","value":"11"}]},
-    {
-      "Order":4,
-      "Id":"override",
-      "Name":"Override",
-      "BitLength":2,
-      "BitOffset":6,
-      "BitStart":6,
-      "Type":"Lookup table",
-      "Signed":false,
-      "EnumValues":[
-        {"name":"No","value":"0"},
-        {"name":"Yes","value":"1"},
-        {"name":"Error","value":"10"},
-        {"name":"Unavailable","value":"11"}]},
-    {
-      "Order":5,
-      "Id":"steeringMode",
-      "Name":"Steering Mode",
-      "BitLength":3,
-      "BitOffset":8,
-      "BitStart":0,
-      "Type":"Lookup table",
-      "Signed":false,
-      "EnumValues":[
-        {"name":"Main Steering","value":"0"},
-        {"name":"Non-Follow-up Device","value":"1"},
-        {"name":"Follow-up Device","value":"10"},
-        {"name":"Heading Control Standalone","value":"11"},
-        {"name":"Heading Control","value":"100"},
-        {"name":"Track Control","value":"101"}]},
-    {
-      "Order":6,
-      "Id":"turnMode",
-      "Name":"Turn Mode",
-      "BitLength":3,
-      "BitOffset":11,
-      "BitStart":3,
-      "Type":"Lookup table",
-      "Signed":false,
-      "EnumValues":[
-        {"name":"Rudder Limit controlled","value":"0"},
-        {"name":"turn rate controlled","value":"1"},
-        {"name":"radius controlled","value":"10"}]},
-    {
-      "Order":7,
-      "Id":"headingReference",
-      "Name":"Heading Reference",
-      "BitLength":2,
-      "BitOffset":14,
-      "BitStart":6,
-      "Type":"Lookup table",
-      "Signed":false,
-      "EnumValues":[
-        {"name":"True","value":"0"},
-        {"name":"Magnetic","value":"1"},
-        {"name":"Error","value":"2"},
-        {"name":"Null","value":"3"}]},
-    {
-      "Order":8,
-      "Id":"reserved",
-      "Name":"Reserved",
-      "BitLength":5,
-      "BitOffset":16,
-      "BitStart":0,
-      "Type":"Binary data",
-      "Signed":false},
-    {
-      "Order":9,
-      "Id":"commandedRudderDirection",
-      "Name":"Commanded Rudder Direction",
-      "BitLength":3,
-      "BitOffset":21,
-      "BitStart":5,
-      "Type":"Lookup table",
-      "Signed":false,
-      "EnumValues":[
-        {"name":"No Order","value":"0"},
-        {"name":"Move to starboard","value":"1"},
-        {"name":"Move to port","value":"10"}]},
-    {
-      "Order":10,
-      "Id":"commandedRudderAngle",
-      "Name":"Commanded Rudder Angle",
-      "BitLength":16,
-      "BitOffset":24,
-      "BitStart":0,
-      "Units":"rad",
-      "Resolution":"0.0001",
-      "Signed":true},
-    {
-      "Order":11,
-      "Id":"headingToSteerCourse",
-      "Name":"Heading-To-Steer (Course)",
-      "BitLength":16,
-      "BitOffset":40,
-      "BitStart":0,
-      "Units":"rad",
-      "Resolution":"0.0001",
-      "Signed":false},
-    {
-      "Order":12,
-      "Id":"track",
-      "Name":"Track",
-      "BitLength":16,
-      "BitOffset":56,
-      "BitStart":0,
-      "Units":"rad",
-      "Resolution":"0.0001",
-      "Signed":false},
-    {
-      "Order":13,
-      "Id":"rudderLimit",
-      "Name":"Rudder Limit",
-      "BitLength":16,
-      "BitOffset":72,
-      "BitStart":0,
-      "Units":"rad",
-      "Resolution":"0.0001",
-      "Signed":false},
-    {
-      "Order":14,
-      "Id":"offHeadingLimit",
-      "Name":"Off-Heading Limit",
-      "BitLength":16,
-      "BitOffset":88,
-      "BitStart":0,
-      "Units":"rad",
-      "Resolution":"0.0001",
-      "Signed":false},
-    {
-      "Order":15,
-      "Id":"radiusOfTurnOrder",
-      "Name":"Radius of Turn Order",
-      "BitLength":16,
-      "BitOffset":104,
-      "BitStart":0,
-      "Units":"rad",
-      "Resolution":"0.0001",
-      "Signed":true},
-    {
-      "Order":16,
-      "Id":"rateOfTurnOrder",
-      "Name":"Rate of Turn Order",
-      "BitLength":16,
-      "BitOffset":120,
-      "BitStart":0,
-      "Units":"rad/s",
-      "Resolution":3.125e-05,
-      "Signed":true},
-    {
-      "Order":17,
-      "Id":"offTrackLimit",
-      "Name":"Off-Track Limit",
-      "BitLength":16,
-      "BitOffset":136,
-      "BitStart":0,
-      "Units":"m",
-      "Signed":true},
-    {
-      "Order":18,
-      "Id":"vesselHeading",
-      "Name":"Vessel Heading",
-      "BitLength":16,
-      "BitOffset":152,
-      "BitStart":0,
-      "Units":"rad",
-      "Resolution":"0.0001",
-      "Signed":false}]
+      "PGN":127999,
+      "Id":"headingTrackControl",
+      "Description":"Heading/Track control",
+      "Type":"Fast",
+      "Complete":true,
+      "Length":21,
+      "RepeatingFields":0,
+      "Fields":[
+        {
+          "Order":1,
+          "Id":"rudderLimitExceeded",
+          "Name":"Rudder Limit Exceeded",
+          "BitLength":2,
+          "BitOffset":0,
+          "BitStart":0,
+          "Type":"Lookup table",
+          "Signed":false,
+          "EnumValues":[
+            {"name":"No","value":"0"},
+            {"name":"Yes","value":"1"},
+            {"name":"Error","value":"10"},
+            {"name":"Unavailable","value":"11"}]},
+        {
+          "Order":2,
+          "Id":"offHeadingLimitExceeded",
+          "Name":"Off-Heading Limit Exceeded",
+          "BitLength":2,
+          "BitOffset":2,
+          "BitStart":2,
+          "Type":"Lookup table",
+          "Signed":false,
+          "EnumValues":[
+            {"name":"No","value":"0"},
+            {"name":"Yes","value":"1"},
+            {"name":"Error","value":"10"},
+            {"name":"Unavailable","value":"11"}]},
+        {
+          "Order":3,
+          "Id":"offTrackLimitExceeded",
+          "Name":"Off-Track Limit Exceeded",
+          "BitLength":2,
+          "BitOffset":4,
+          "BitStart":4,
+          "Type":"Lookup table",
+          "Signed":false,
+          "EnumValues":[
+            {"name":"No","value":"0"},
+            {"name":"Yes","value":"1"},
+            {"name":"Error","value":"10"},
+            {"name":"Unavailable","value":"11"}]},
+        {
+          "Order":4,
+          "Id":"override",
+          "Name":"Override",
+          "BitLength":2,
+          "BitOffset":6,
+          "BitStart":6,
+          "Type":"Lookup table",
+          "Signed":false,
+          "EnumValues":[
+            {"name":"No","value":"0"},
+            {"name":"Yes","value":"1"},
+            {"name":"Error","value":"10"},
+            {"name":"Unavailable","value":"11"}]},
+        {
+          "Order":5,
+          "Id":"steeringMode",
+          "Name":"Steering Mode",
+          "BitLength":3,
+          "BitOffset":8,
+          "BitStart":0,
+          "Type":"Lookup table",
+          "Signed":false,
+          "EnumValues":[
+            {"name":"Main Steering","value":"0"},
+            {"name":"Non-Follow-up Device","value":"1"},
+            {"name":"Follow-up Device","value":"10"},
+            {"name":"Heading Control Standalone","value":"11"},
+            {"name":"Heading Control","value":"100"},
+            {"name":"Track Control","value":"101"}]},
+        {
+          "Order":6,
+          "Id":"turnMode",
+          "Name":"Turn Mode",
+          "BitLength":3,
+          "BitOffset":11,
+          "BitStart":3,
+          "Type":"Lookup table",
+          "Signed":false,
+          "EnumValues":[
+            {"name":"Rudder Limit controlled","value":"0"},
+            {"name":"turn rate controlled","value":"1"},
+            {"name":"radius controlled","value":"10"}]},
+        {
+          "Order":7,
+          "Id":"headingReference",
+          "Name":"Heading Reference",
+          "BitLength":2,
+          "BitOffset":14,
+          "BitStart":6,
+          "Type":"Lookup table",
+          "Signed":false,
+          "EnumValues":[
+            {"name":"True","value":"0"},
+            {"name":"Magnetic","value":"1"},
+            {"name":"Error","value":"2"},
+            {"name":"Null","value":"3"}]},
+        {
+          "Order":8,
+          "Id":"reserved",
+          "Name":"Reserved",
+          "BitLength":5,
+          "BitOffset":16,
+          "BitStart":0,
+          "Type":"Binary data",
+          "Signed":false},
+        {
+          "Order":9,
+          "Id":"commandedRudderDirection",
+          "Name":"Commanded Rudder Direction",
+          "BitLength":3,
+          "BitOffset":21,
+          "BitStart":5,
+          "Type":"Lookup table",
+          "Signed":false,
+          "EnumValues":[
+            {"name":"No Order","value":"0"},
+            {"name":"Move to starboard","value":"1"},
+            {"name":"Move to port","value":"10"}]},
+        {
+          "Order":10,
+          "Id":"commandedRudderAngle",
+          "Name":"Commanded Rudder Angle",
+          "BitLength":16,
+          "BitOffset":24,
+          "BitStart":0,
+          "Units":"rad",
+          "Resolution":"0.0001",
+          "Signed":true},
+        {
+          "Order":11,
+          "Id":"headingToSteerCourse",
+          "Name":"Heading-To-Steer (Course)",
+          "BitLength":16,
+          "BitOffset":40,
+          "BitStart":0,
+          "Units":"rad",
+          "Resolution":"0.0001",
+          "Signed":false},
+        {
+          "Order":12,
+          "Id":"track",
+          "Name":"Track",
+          "BitLength":16,
+          "BitOffset":56,
+          "BitStart":0,
+          "Units":"rad",
+          "Resolution":"0.0001",
+          "Signed":false},
+        {
+          "Order":13,
+          "Id":"rudderLimit",
+          "Name":"Rudder Limit",
+          "BitLength":16,
+          "BitOffset":72,
+          "BitStart":0,
+          "Units":"rad",
+          "Resolution":"0.0001",
+          "Signed":false},
+        {
+          "Order":14,
+          "Id":"offHeadingLimit",
+          "Name":"Off-Heading Limit",
+          "BitLength":16,
+          "BitOffset":88,
+          "BitStart":0,
+          "Units":"rad",
+          "Resolution":"0.0001",
+          "Signed":false},
+        {
+          "Order":15,
+          "Id":"radiusOfTurnOrder",
+          "Name":"Radius of Turn Order",
+          "BitLength":16,
+          "BitOffset":104,
+          "BitStart":0,
+          "Units":"rad",
+          "Resolution":"0.0001",
+          "Signed":true},
+        {
+          "Order":16,
+          "Id":"rateOfTurnOrder",
+          "Name":"Rate of Turn Order",
+          "BitLength":16,
+          "BitOffset":120,
+          "BitStart":0,
+          "Units":"rad/s",
+          "Resolution":3.125e-05,
+          "Signed":true},
+        {
+          "Order":17,
+          "Id":"offTrackLimit",
+          "Name":"Off-Track Limit",
+          "BitLength":16,
+          "BitOffset":136,
+          "BitStart":0,
+          "Units":"m",
+          "Signed":true},
+        {
+          "Order":18,
+          "Id":"vesselHeading",
+          "Name":"Vessel Heading",
+          "BitLength":16,
+          "BitOffset":152,
+          "BitStart":0,
+          "Units":"rad",
+          "Resolution":"0.0001",
+          "Signed":false}]
+    }
+  ]
 }
