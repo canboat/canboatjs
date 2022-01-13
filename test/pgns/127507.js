daf5ad31bc4747ae842099c702ff253a3516990e
nmbath <73959588+nmbath@users.noreply.github.com>
2022-01-13 19:52:30 +0000

    tests: add test for PGN127507 - (#176)

---
+++ test/pgns/127507.js
@@ -0,0 +1,4 @@
+module.exports = [{
+"expected": {"timestamp":"2016-02-28T19:57:02.829Z","prio":6,"src":129,"dst":255,"pgn":127507,"description":"Charger Status","fields":{"Instance":114,"Battery Instance":6,"Operating State":"Disabled","Charge Mode":"Standalone","Enabled":"Off"}},
+"input": "2016-02-28T19:57:02.829Z,6,127507,129,255,6,72,06,08,fc,ff,ff"
+}]
