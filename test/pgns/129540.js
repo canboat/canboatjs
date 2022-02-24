43b047056e7d62ea61df7c45a4877d6179de4e18
Scott Bender <scott@scottbender.net>
2022-02-24 10:58:35 -0500

    tests: add test for multi line actisense

--- test/pgns/129540.js
+++ test/pgns/129540.js
@@ -1,4 +1,31 @@
 module.exports = [{
   "expected": {"timestamp":"2017-04-15T14:57:58.469Z","prio":6,"src":3,"dst":255,"pgn":129540,"description":"GNSS Sats in View","fields":{"SID":184,"Sats in View":18,"list":[{"PRN":3,"Elevation":0.5585,"Azimuth":5.3407,"SNR":31,"Range residuals":0,"Status":"Used+Diff"},{"PRN":4,"Elevation":1.0297,"Azimuth":3.002,"SNR":28,"Range residuals":0,"Status":"Used+Diff"},{"PRN":10,"Elevation":0.1047,"Azimuth":2.8972,"SNR":28,"Range residuals":0,"Status":"Used+Diff"},{"PRN":14,"Elevation":1.1519,"Azimuth":1.5359,"SNR":30,"Range residuals":0,"Status":"Used+Diff"},{"PRN":16,"Elevation":0.4014,"Azimuth":3.4383,"SNR":33,"Range residuals":0,"Status":"Used+Diff"},{"PRN":22,"Elevation":0.8029,"Azimuth":4.8346,"SNR":31,"Range residuals":0,"Status":"Used+Diff"},{"PRN":25,"Elevation":0.4538,"Azimuth":0.7679,"SNR":33,"Range residuals":0,"Status":"Used+Diff"},{"PRN":26,"Elevation":0.9076,"Azimuth":3.2463,"SNR":26,"Range residuals":0,"Status":"Used+Diff"},{"PRN":31,"Elevation":1.2217,"Azimuth":0.2269,"SNR":31,"Range residuals":0,"Status":"Used+Diff"},{"PRN":67,"Elevation":0.7505,"Azimuth":2.7751,"SNR":32,"Range residuals":0,"Status":"Used+Diff"},{"PRN":68,"Elevation":1.309,"Azimuth":5.2709,"SNR":34,"Range residuals":0,"Status":"Used+Diff"},{"PRN":69,"Elevation":0.4014,"Azimuth":5.7072,"SNR":26,"Range residuals":0,"Status":"Used+Diff"},{"PRN":77,"Elevation":0.5934,"Azimuth":0.6109,"SNR":29,"Range residuals":0,"Status":"Used+Diff"},{"PRN":78,"Elevation":1.2392,"Azimuth":5.2011,"SNR":27,"Range residuals":0,"Status":"Used+Diff"},{"PRN":79,"Elevation":0.6632,"Azimuth":4.2761,"SNR":35,"Range residuals":0,"Status":"Used+Diff"},{"PRN":46,"Elevation":0.6632,"Azimuth":3.6826,"SNR":30,"Range residuals":0,"Status":"Used+Diff"},{"PRN":48,"Elevation":0.2967,"Azimuth":4.311,"SNR":29,"Range residuals":0,"Status":"Used+Diff"},{"PRN":51,"Elevation":0.6109,"Azimuth":3.9095,"SNR":35,"Range residuals":0,"Status":"Used+Diff"}]}},
   "input": "2017-04-15T14:57:58.469Z,6,129540,3,255,219,b8,ff,12,03,d1,15,9f,d0,1c,0c,00,00,00,00,f5,04,39,28,44,75,f0,0a,00,00,00,00,f5,0a,17,04,2c,71,f0,0a,00,00,00,00,f5,0e,ff,2c,ff,3b,b8,0b,00,00,00,00,f5,10,ae,0f,4f,86,e4,0c,00,00,00,00,f5,16,5d,1f,da,bc,1c,0c,00,00,00,00,f5,19,ba,11,ff,1d,e4,0c,00,00,00,00,f5,1a,74,23,cf,7e,28,0a,00,00,00,00,f5,1f,b9,2f,dd,08,1c,0c,00,00,00,00,f5,43,51,1d,67,6c,80,0c,00,00,00,00,f5,44,22,33,e5,cd,48,0d,00,00,00,00,f5,45,ae,0f,f0,de,28,0a,00,00,00,00,f5,4d,2e,17,dd,17,54,0b,00,00,00,00,f5,4e,68,30,2b,cb,8c,0a,00,00,00,00,f5,4f,e8,19,09,a7,ac,0d,00,00,00,00,f5,2e,e8,19,da,8f,b8,0b,00,00,00,00,f5,30,97,0b,66,a8,54,0b,00,00,00,00,f5,33,dd,17,b7,98,ac,0d,00,00,00,00,f5"
-}]
+}, {
+  "expected": {"prio":6,"pgn":129540,"dst":255,"src":1,"timestamp":"2020-08-16T09:00:00.364Z","fields":{"SID":82,"Mode":2,"Sats in View":12,"list":[{"PRN":7,"Elevation":0.2443,"Azimuth":5.6199,"SNR":33,"Range residuals":0,"Status":"Used"},{"PRN":10,"Elevation":0.6632,"Azimuth":3.0543,"SNR":35,"Range residuals":0,"Status":"Used"},{"PRN":13,"Elevation":0.3141,"Azimuth":0.5934,"SNR":37,"Range residuals":0,"Status":"Used"},{"PRN":15,"Elevation":0.4363,"Azimuth":1.1693,"SNR":39,"Range residuals":0,"Status":"Used"},{"PRN":16,"Elevation":0.8028,"Azimuth":4.0142,"SNR":31,"Range residuals":0,"Status":"Used"},{"PRN":18,"Elevation":0.8203,"Azimuth":1.4311,"SNR":32,"Range residuals":0,"Status":"Used"},{"PRN":20,"Elevation":1.0471,"Azimuth":2.3038,"SNR":36,"Range residuals":0,"Status":"Used"},{"PRN":21,"Elevation":0.9599,"Azimuth":4.2935,"SNR":28,"Range residuals":0,"Status":"Used"},{"PRN":26,"Elevation":0.3316,"Azimuth":3.4557,"SNR":35,"Range residuals":0,"Status":"Used"},{"PRN":27,"Elevation":1.0471,"Azimuth":4.8869,"SNR":35,"Range residuals":0,"Status":"Used"},{"PRN":8,"Elevation":0.4886,"Azimuth":5.1661,"SNR":24,"Range residuals":0,"Status":"Used"},{"PRN":87,"Elevation":1.2217,"Azimuth":5.2359,"SNR":28,"Range residuals":0,"Status":"Used"},{"SNR":2.55}]},"description":"GNSS Sats in View"},
+  "input": [
+    '2020-08-16T09:00:00.333Z,6,129540,1,255,8,20,93,52,fe,0c,07,8b,09',
+    '2020-08-16T09:00:00.334Z,6,129540,1,255,8,21,87,db,e4,0c,00,00,00',
+    '2020-08-16T09:00:00.334Z,6,129540,1,255,8,22,00,f2,0a,e8,19,4f,77',
+    '2020-08-16T09:00:00.335Z,6,129540,1,255,8,23,ac,0d,00,00,00,00,f2',
+    '2020-08-16T09:00:00.335Z,6,129540,1,255,8,24,0d,45,0c,2e,17,74,0e',
+    '2020-08-16T09:00:00.336Z,6,129540,1,255,8,25,00,00,00,00,f2,0f,0b',
+    '2020-08-16T09:00:00.336Z,6,129540,1,255,8,26,11,ad,2d,3c,0f,00,00',
+    '2020-08-16T09:00:00.337Z,6,129540,1,255,8,27,00,00,f2,10,5c,1f,ce',
+    '2020-08-16T09:00:00.337Z,6,129540,1,255,8,28,9c,1c,0c,00,00,00,00',
+    '2020-08-16T09:00:00.338Z,6,129540,1,255,8,29,f2,12,0b,20,e7,37,80',
+    '2020-08-16T09:00:00.339Z,6,129540,1,255,8,2a,0c,00,00,00,00,f2,14',
+    '2020-08-16T09:00:00.358Z,6,129540,1,255,8,2b,e7,28,fe,59,10,0e,00',
+    '2020-08-16T09:00:00.359Z,6,129540,1,255,8,2c,00,00,00,f2,15,7f,25',
+    '2020-08-16T09:00:00.360Z,6,129540,1,255,8,2d,b7,a7,f0,0a,00,00,00',
+    '2020-08-16T09:00:00.360Z,6,129540,1,255,8,2e,00,f2,1a,f4,0c,fd,86',
+    '2020-08-16T09:00:00.361Z,6,129540,1,255,8,2f,ac,0d,00,00,00,00,f2',
+    '2020-08-16T09:00:00.361Z,6,129540,1,255,8,30,1b,e7,28,e5,be,ac,0d',
+    '2020-08-16T09:00:00.361Z,6,129540,1,255,8,31,00,00,00,00,f2,08,16',
+    '2020-08-16T09:00:00.362Z,6,129540,1,255,8,32,13,cd,c9,60,09,00,00',
+    '2020-08-16T09:00:00.363Z,6,129540,1,255,8,33,00,00,f2,57,b9,2f,87',
+    '2020-08-16T09:00:00.363Z,6,129540,1,255,8,34,cc,f0,0a,00,00,00,00',
+    '2020-08-16T09:00:00.364Z,6,129540,1,255,8,35,f2,ff,ff,ff,ff,ff,ff'
+  ]
+}
+]
