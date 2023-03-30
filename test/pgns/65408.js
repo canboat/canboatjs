a300e13523ede2917df40121c2ba7ddc72c5e716
Scott Bender <scott@scottbender.net>
2023-03-30 12:39:27 -0400

    tests: fix short pgns

--- test/pgns/65408.js
+++ test/pgns/65408.js
@@ -1,4 +1,4 @@
 module.exports = [{
   "expected": {"timestamp":"2016-02-28T19:57:02.828Z","prio":7,"src":36,"dst":255,"pgn":65408,"description":"Airmar: Depth Quality Factor","fields":{"Manufacturer Code":"Airmar","Industry Code":"Marine Industry","SID":22,"Depth Quality Factor":"Quality 100%"}},
-  "input": "2016-02-28T19:57:02.828Z,7,65408,36,255,4,87,98,16,fa"
+  "input": "2016-02-28T19:57:02.828Z,7,65408,36,255,8,87,98,16,fa,ff,ff,ff,ff"
 }]
