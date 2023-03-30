c44c8be619f47e1049fa85188b2825e8b41267fb
Scott Bender <scott@scottbender.net>
2023-03-30 17:05:14 -0400

    tests: add tests for 129798 and 127497

---
+++ test/pgns/127497.js
@@ -0,0 +1,4 @@
+module.exports = [{
+  "expected": {"prio":5,"pgn":127497,"dst":255,"src":57,"timestamp":"2020-03-31T02:34:18.529Z","fields":{"Instance":224,"Trip Fuel Used":9,"Fuel Rate, Average":-3100.2,"Instantaneous Fuel Economy":-0.1},"description":"Trip Parameters, Engine"},
+  "input": "2020-03-31T02:34:18.529Z,5,127497,57,255,9,e0,09,00,e6,86,ff,7f,ff,ff"
+}]
