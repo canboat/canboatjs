ab96bcbb0cfe164c89d537e9419d619a72551e5a
Scott Bender <scott@scottbender.net>
2023-04-01 15:26:47 -0400

    fix: use Resolution in time calculations

--- test/pgns/127506.js
+++ test/pgns/127506.js
@@ -1,4 +1,5 @@
 module.exports = [{
-  "expected": {"timestamp":"2016-02-28T19:57:02.829Z","prio":6,"src":60,"dst":255,"pgn":127506,"description":"DC Detailed Status","fields":{"Instance":3,"State of Charge":100}},
-  "input": "2016-02-28T19:57:02.829Z,6,127506,60,255,11,ff,03,ff,64,ff,ff,ff,ff,ff,ff,ff"
+  "expected": {"timestamp":"2023-04-01T17:28:01.128Z","prio":2,"src":60,"dst":255,"pgn":127506,"description":"DC Detailed Status","fields":{"Instance":3,"State of Charge":100, "Time Remaining": "00:20:00"}},
+  "input": "2023-04-01T17:28:01.128Z,2,127506,60,255,11,ff,03,ff,64,ff,14,00,ff,ff,ff,ff"
+  //"2016-02-28T19:57:02.829Z,6,127506,60,255,11,ff,03,ff,64,ff,ff,ff,ff,ff,ff,ff"
 }]
