17aa3e59a2bdf69ab943c995f7c12928edd9efb8
Scott Bender <scott@scottbender.net>
2023-09-24 20:28:41 -0400

    tests: added tests for 126208 130845 (#254)

---
+++ test/pgns/130845.js
@@ -0,0 +1,4 @@
+module.exports = [{
+  "expected": {"timestamp":"2023-09-20T14:11:11.577Z","prio":3,"src":2,"dst":255,"pgn":130845,"description":"Simnet: Key Value","fields":{"Manufacturer Code":"Simrad","Industry Code":"Marine Industry","Display Group":"Default","Key":"Backlight level","MinLength":1,"Value":88, "Spare": 0, "Address": null, "Repeat Indicator": null, "Reserved1": null, "Reserved2": null}},
+  "input": "2023-09-20T14:11:11.577Z,3,130845,2,255,11,41,9f,ff,ff,01,ff,ff,12,00,01,58"
+}]
