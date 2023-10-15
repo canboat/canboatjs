c23b1f936cc8113ea42611dbde95e7b00ed4a790
Scott Bender <scott@scottbender.net>
2023-10-15 15:01:07 -0400

    fix: handle 0183 messages with v4 tag blocks (#258)
    fixes #257

--- test/pcdin.js
+++ test/pcdin.js
@@ -50,4 +50,49 @@ describe('from pcdin data converts', function () {
 
     fromPgn.parseString(pcdin)
   })
+
+
+  it(`from 127257 converts with tag blocks`, function (done) {
+    var pcdin = '\\s:serial,c:1696759212*3E\\$PCDIN,01F119,00000000,0F,2AAF00D1067414FF*59'
+    var expected = {
+      "pgn":127257,
+      "timestamp": "2010-01-01T00:00:00.000Z",
+      "timer": 1262304000000,
+      "src":15,
+      "dst":255,
+      "prio":0,
+      "fields":{
+        "SID": 42,
+        "Pitch": 0.1745,
+        "Roll": 0.5236,
+        "Yaw": 0.0175
+      },
+      "description":"Attitude"
+    }
+
+    var fromPgn = new FromPgn()
+
+    fromPgn.on('error', (pgn, error) => {
+      console.error(`Error parsing ${pgn.pgn} ${error}`)
+      console.error(error.stack)
+      done(error)
+    })
+
+    fromPgn.on('warning', (pgn, warning) => {
+      done(new Error(`${pgn.pgn} ${warning}`))
+    })
+
+    fromPgn.on('pgn', (pgn) => {
+      try {
+        //console.log(JSON.stringify(pgn))
+        delete pgn.input
+        pgn.should.jsonEqual(expected)
+        done()
+      } catch ( e ) {
+        done(e)
+      }
+    })
+
+    fromPgn.parseString(pcdin)
+  })
 })
