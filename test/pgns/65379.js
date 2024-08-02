2c4e2b0029fcf3da23ba0bcf5c1fe56e07b6724a
Scott Bender <scott@scottbender.net>
2024-08-02 09:53:00 -0400

    tests: sync test for 65379 to canboat 5.1.0 pgn definitions (#278)

--- test/pgns/65379.js
+++ test/pgns/65379.js
@@ -1,4 +1,4 @@
 module.exports = [{
-  "expected": {"timestamp":"2017-04-15T14:57:59.360Z","prio":7,"src":204,"dst":255,"pgn":65379,"description":"Seatalk: Pilot Mode","fields":{"Manufacturer Code":"Raymarine","Industry Code":"Marine Industry","Pilot Mode":129,"Sub Mode":1,"Pilot Mode Data":0,"Reserved2": 16777215,"Reserved1":null}},
-  "input": "2017-04-15T14:57:59.360Z,7,65379,204,255,8,3b,9f,81,01,00,ff,ff,ff"
+  "expected": {"timestamp":"2024-08-02T13:42:25.815Z","prio":7,"src":204,"dst":255,"pgn":65379,"description":"Seatalk: Pilot Mode","fields":{"Manufacturer Code":"Raymarine","Industry Code":"Marine Industry","Pilot Mode":"Auto, compass commanded","Sub Mode":0,"Pilot Mode Data":0,"Reserved2": null,"Reserved1":null}},
+  "input": "2024-08-02T13:42:25.815Z,7,65379,204,255,8,3b,9f,40,00,00,00,00,ff"
 }]
