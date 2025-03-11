24819b9e382adc27e06255341c0fc0c648231af5
Scott Bender <scott@scottbender.net>
2025-03-10 20:27:59 -0400

    fix: issues with STRING_FIX field type decoding (#294)

--- test/pgns/129044.js
+++ test/pgns/129044.js
@@ -1,4 +1,10 @@
 module.exports = [{
   "expected": {"timestamp":"2017-04-15T14:58:06.474Z","prio":6,"src":3,"dst":255,"pgn":129044,"description":"Datum","fields":{"Local Datum":"W84","Delta Latitude": 0.0000000,"Delta Longitude": 0.0000000,"Delta Altitude":0.000000,"Reference Datum":"W84"}},
   "input": "2017-04-15T14:58:06.474Z,6,129044,3,255,20,57,38,34,ff,00,00,00,00,00,00,00,00,00,00,00,00,57,38,34,ff",
+},{
+
+  //Testing STRING_FIX with a zero byte
+  "expected": {"timestamp":"2017-04-15T14:58:06.474Z","prio":6,"src":3,"dst":255,"pgn":129044,"description":"Datum","fields":{"Local Datum":"W8","Delta Latitude": 0.0000000,"Delta Longitude": 0.0000000,"Delta Altitude":0.000000,"Reference Datum":"W84"}},
+  "input": "2017-04-15T14:58:06.474Z,6,129044,3,255,20,57,38,00,ff,00,00,00,00,00,00,00,00,00,00,00,00,57,38,34,ff",
+  "skipEncoderTest": true,
 }]
