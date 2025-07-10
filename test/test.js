1123df30abc6363d51e6c53d356e021c341e4929
Scott Bender <scott@scottbender.net>
2025-07-10 07:32:59 -0400

    fix: formatting

--- test/test.js
+++ test/test.js
@@ -24,7 +24,6 @@ describe('from pgn test data converts', function () {
   _.keys(testData).forEach((key) => {
     var dataList = testData[key]
 
-
     dataList.forEach((data, idx) => {
       it(`from pgn ${key} (${data.expected.description}) (${idx}) converts`, function (done) {
         let format = typeof data.format !== 'undefined' ? data.format : 1
@@ -87,27 +86,23 @@ describe('to pgn test data converts', function () {
     var dataList = testData[key]
 
     dataList.forEach((test, idx) => {
-        if (
-          test.disabled ||
-          test.skipEncoderTest ||
-          Array.isArray(test.input)
-        ) {
-          return
-        }
+      if (test.disabled || test.skipEncoderTest || Array.isArray(test.input)) {
+        return
+      }
 
       it(`to pgn ${key} (${test.expected.description}) (${idx}) converts`, function (done) {
         try {
           var data = toPgn(test.expected)
           var str = encodeActisense({ pgn: test.expected.pgn, data })
-          
+
           var expected = test.input.split(',')
           var result = str.split(',')
-          
+
           result[2].should.equal(expected[2])
 
           result.slice(5).should.deep.equal(expected.slice(5))
           done()
-        } catch ( err ) {
+        } catch (err) {
           done(err)
         }
       })
