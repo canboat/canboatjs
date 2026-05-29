09c8dcdb54f9efbfd593f228cfb298ace8e923ec
Scott Bender <scott@scottbender.net>
2026-05-29 09:49:37 -0400

    fix: return the enum value if there for lookups that are max value (#440)

---
+++ test/pgns/127503.js
@@ -0,0 +1,33 @@
+module.exports = [
+  {
+    expected: {
+      pgn: 127503,
+      prio: 2,
+      src: 0,
+      dst: 255,
+      timestamp: '2026-05-29T12:17:02.604Z',
+      description: 'AC Input Status',
+      fields: {
+        instance: 0,
+        numberOfLines: 1,
+        list: [
+          {
+            line: 'Line 1',
+            acceptability: 'Good',
+            voltage: 121.02,
+            current: 0.6,
+            frequency: 60.31,
+            breakerSize: 30,
+            realPower: 4,
+            reactivePower: 4,
+            powerFactor: 1,
+            reserved: null
+          }
+        ]
+      }
+    },
+
+    input:
+      '2026-05-29T12:17:02.604Z,2,127503,0,255,20,00,01,fc,46,2f,06,00,8f,17,2c,01,04,00,00,00,04,00,00,00,64'
+  }
+]
